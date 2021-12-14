efb5990e31c2c68a04eca0c4be909f248127ddeb
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-12-14 16:16:05 -0600

    Made basic folder/file structures.

---
+++ src/components/Blocks/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blocks = () => {
+  return <h1>Blocks Test</h1>
+}
+
+export default Blocks;
---
+++ src/components/Grid/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Blocks from "../Blocks";
+
+const Grid = () => {
+  return <div>
+            <h1>Grid Test</h1>
+            <Blocks />
+          </div>
+
+}
+
+export default Grid;
